c254686c059d6b6da8e20eeadb7faae7c760aacc
Carolyn-qxr <3147037280@qq.com>
2020-11-09 18:43:03 +0800

    使用隐式启动实现打电话功能

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button start;
+    @Override
+    /*还需要动态修改app的电话权限*/
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start = findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+
+            }
+        });
+    }
+}
